299dbd15ea0a422768d6c8cf2124a8924f3b936e
Shawn CK.P <snowy213@outlook.com>
2021-12-23 15:56:34 -0800

    function practice using go

--- main.go
+++ main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Hello, World!")
+	foods := []string{"food", "pizza", "pasta", "ramen"}
+	fmt.Printf("%v\n", foods)
+	newFoods := append(foods, "tomato")
+	fmt.Printf("%v\n", newFoods)
 }
