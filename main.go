31c7fdb23d906350921ccb7b15ca5a4f661b47a8
Shawn CK.P <snowy213@outlook.com>
2021-12-24 14:49:26 -0800

    the first block

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
-
-	"github.com/Nomankey/chancoin.git/person"
 )
 
+type block struct {
+	data string
+	hash string
+	prevHash string
+}
 
 func main() {
-	nomankey := person.Person{}
-	nomankey.SeeDetails("chan", 25)
-	fmt.Println("\nSee main",nomankey)
+	genesisBlock := block{"Genesis Block", "", ""}
+	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash))
+	hexHash := fmt.Sprintf("%x", hash)
+	genesisBlock.hash = hexHash
+	fmt.Println(genesisBlock)
 }
