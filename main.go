b8ff33f73d45a2296ed4628c4ca7cf8921a2ad71
Shawn CK.P <snowy213@outlook.com>
2021-12-30 17:30:26 -0800

    pages and partials added to the template

--- main.go
+++ main.go
@@ -12,18 +12,18 @@ import (
 const port string = ":4000"
 
 type homeData struct {
-	pageTitle string
-	Blocks []*blockchain.Block
+	PageTitle string
+	Blocks    []*blockchain.Block
 }
 
 func home(rw http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/home.html"))
+	tmpl := template.Must(template.ParseFiles("templates/home.gohtml"))
 	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
 	tmpl.Execute(rw, data)
 }
 
 func main() {
 	http.HandleFunc("/", home)
-	fmt.Printf("listening http://localhost%s\n", port)
+	fmt.Printf("listening: http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
